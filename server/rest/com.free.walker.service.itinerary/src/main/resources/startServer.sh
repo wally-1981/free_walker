19f32a39a51dc37f036cb188778e4d6c6a82e59e
free-walker <wally.1981@yahoo.com>
2014-12-21 17:45:21 +0800

    modify server start for Linux support
    Signed-off-by: free-walker <wally.1981@yahoo.com>

--- server/rest/com.free.walker.service.itinerary/src/main/resources/startServer.sh
+++ server/rest/com.free.walker.service.itinerary/src/main/resources/startServer.sh
@@ -1,10 +1,9 @@
 # Please ensure install and start MongoDB and MySQL before launching this server in Prod mode.
-#
+
 # Start MySQL: (The path to mysqld may vary depending on the install location of MySQL on your system.)
-# /opt/MySQL/MySQL Server 5.6/bin/mysqld
-#
+service mysql start
 
 # Start MongoDB: (The path to mongod may vary depending on the install location of MongoDB on your system.)
-/opt/mongo_server/bin/mongod -dbpath /opt/mongo_server/data
+service mongod start
 
 java -jar ./com.free.walker.service.itinerary-0.0.3.jar -Prod
--- server/rest/com.free.walker.service.itinerary/src/main/resources/startServerDevo.sh
+++ server/rest/com.free.walker.service.itinerary/src/main/resources/startServerDevo.sh
@@ -1,7 +1,6 @@
 # Please ensure install and start MySQL before launching this server in Devo mode.
-#
+
 # Start MySQL: (The path to mysqld may vary depending on the install location of MySQL on your system.)
-# /opt/MySQL/MySQL Server 5.6/bin/mysqld
-#
+service mysql start
 
 java -jar ./com.free.walker.service.itinerary-0.0.3.jar -Devo
