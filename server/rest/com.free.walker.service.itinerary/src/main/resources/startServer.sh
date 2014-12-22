f44c0283f06b95314f398e3962f0db7bf82e735c
free-walker <wally.1981@yahoo.com>
2014-12-22 10:36:41 +0800

    remove misleading comment
    Signed-off-by: free-walker <wally.1981@yahoo.com>

--- server/rest/com.free.walker.service.itinerary/src/main/resources/startServer.sh
+++ server/rest/com.free.walker.service.itinerary/src/main/resources/startServer.sh
@@ -1,9 +1,9 @@
 # Please ensure install and start MongoDB and MySQL before launching this server in Prod mode.
 
-# Start MySQL: (The path to mysqld may vary depending on the install location of MySQL on your system.)
-service mysql start
+# Start MySQL:
+#service mysql start
 
-# Start MongoDB: (The path to mongod may vary depending on the install location of MongoDB on your system.)
-service mongod start
+# Start MongoDB:
+#service mongod start
 
 java -jar ./com.free.walker.service.itinerary-0.0.3.jar -Prod
--- server/rest/com.free.walker.service.itinerary/src/main/resources/startServerDevo.sh
+++ server/rest/com.free.walker.service.itinerary/src/main/resources/startServerDevo.sh
@@ -1,6 +1,6 @@
 # Please ensure install and start MySQL before launching this server in Devo mode.
 
-# Start MySQL: (The path to mysqld may vary depending on the install location of MySQL on your system.)
-service mysql start
+# Start MySQL:
+#service mysql start
 
 java -jar ./com.free.walker.service.itinerary-0.0.3.jar -Devo
