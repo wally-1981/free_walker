d7aed575de6453c9ec1a29cc595cd8c74995562a
free-walker <wally.1981@yahoo.com>
2014-12-30 17:14:55 +0800

    add StringPair for easy DAO implementation
    Signed-off-by: free-walker <wally.1981@yahoo.com>

---
+++ server/rest/com.free.walker.service.itinerary/src/main/java/com/free/walker/service/itinerary/basic/StringPair.java
@@ -0,0 +1,26 @@
+package com.free.walker.service.itinerary.basic;
+
+public class StringPair {
+    private String primary;
+    private String secondary;
+
+    public StringPair() {
+        ;
+    }
+
+    public String getPrimary() {
+        return primary;
+    }
+
+    public void setPrimary(String primary) {
+        this.primary = primary;
+    }
+
+    public String getSecondary() {
+        return secondary;
+    }
+
+    public void setSecondary(String secondary) {
+        this.secondary = secondary;
+    }
+}
