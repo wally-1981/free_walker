40e5d83b96aa93f90a8452819586eabdca8c71ce
free-walker <wally.1981@yahoo.com>
2015-02-07 22:11:23 +0800

    automate the setup process of elasticsearch server.
    Signed-off-by: free-walker <wally.1981@yahoo.com>

---
+++ server/rest/com.free.walker.service.itinerary/src/main/resources/db/setup.sh
@@ -0,0 +1,11 @@
+# Please ensure install curl, http://curl.haxx.se, before executing this setup script.
+#
+# This script will setup the foundation data for elasticsearch server.
+
+echo "Please ensure install curl, http://curl.haxx.se, before executing this setup script. Otherwise, its execution will fail."
+
+curl -X POST -d @query_template/test_template.mustache http://localhost:9200/_search/template/test_template --header "content-type:application/json"
+
+curl -X POST -d @query_template/product_departure.mustache http://localhost:9200/_search/template/product_departure --header "content-type:application/json"
+
+curl -X POST -d @query_template/product_destination.mustache http://localhost:9200/_search/template/product_destination --header "content-type:application/json"
