f1fbc61a4da917e7a49536d1b91b38b2c7de5f5c
wally.1981 <wally.1981@yahoo.com>
2015-07-30 00:27:47 +0800

    upgrade the build to 0.0.15, fix some break in UT for resource service, fix several warning during maven build.
    Signed-off-by: wally.1981 <wally.1981@yahoo.com>

--- server/rest/com.free.walker.service.itinerary/src/test/java/com/free/walker/service/itinerary/rest/AbstractResourceServiceTest.java
+++ server/rest/com.free.walker.service.itinerary/src/test/java/com/free/walker/service/itinerary/rest/AbstractResourceServiceTest.java
@@ -9,7 +9,9 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import javax.json.Json;
+import javax.json.JsonArray;
 import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.ws.rs.ProcessingException;
 
 import org.apache.cxf.helpers.IOUtils;
@@ -17,6 +19,7 @@ import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.eclipse.jetty.http.HttpStatus;
 import org.junit.Before;
 import org.junit.Rule;
@@ -80,6 +83,45 @@ public abstract class AbstractResourceServiceTest extends BaseConfigurationProvi
                     assertTrue(syncResult.getInt(Introspection.JSONKeys.SYNC_ADD_NUMBER) >= 0);
                     assertTrue(syncResult.getInt(Introspection.JSONKeys.SYNC_UPDATE_NUMBER) >= 0);
                     assertTrue(syncResult.getInt(Introspection.JSONKeys.SYNC_DELETE_NUMBER) >= 0);
+                } else if (statusCode == HttpStatus.UNAUTHORIZED_401) {
+                    LOG.error(IOUtils.toString(response.getEntity().getContent()));
+                    assertTrue(false);
+                } else {
+                    JsonObject error = Json.createReader(response.getEntity().getContent()).readObject();
+                    throw new ProcessingException(error.toString());
+                }
+            } catch (IOException e) {
+                throw new ProcessingException(e);
+            } finally {
+                put.abort();
+            }
+        }
+
+        /*
+         * 检索所有Resource。
+         */
+        {
+            JsonObjectBuilder searchCriteria = Json.createObjectBuilder();
+            searchCriteria.add(Introspection.JSONKeys.TERM, "*");
+            searchCriteria.add(Introspection.JSONKeys.TEMPLATE, Introspection.JSONValues.RESOURCE_TEMPLATE_AS_INT);
+            searchCriteria.add(Introspection.JSONKeys.PAGE_NUM, 10);
+            searchCriteria.add(Introspection.JSONKeys.PAGE_SIZE, 10);
+
+            HttpPut put = new HttpPut();
+            put.setEntity(new StringEntity(searchCriteria.build().toString(), ContentType.APPLICATION_JSON));
+            put.setURI(new URI(resourceServiceUrlStr + "resources/"));
+            put.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+            try {
+                HttpResponse response = agencyClient.execute(put);
+                int statusCode = response.getStatusLine().getStatusCode();
+                if (statusCode == HttpStatus.OK_200) {
+                    JsonObject resources = Json.createReader(response.getEntity().getContent()).readObject();
+                    assertNotNull(resources);
+                    long hitsNumber = resources.getJsonNumber(Introspection.JSONKeys.TOTAL_HITS_NUMBER).longValue();
+                    JsonArray hits = resources.getJsonArray(Introspection.JSONKeys.HITS);
+                    assertTrue(hitsNumber > 0);
+                    assertNotNull(hits);
+                    assertTrue(hits.size() > 0);
                 } else if (statusCode == HttpStatus.UNAUTHORIZED_401) {
                     LOG.error(IOUtils.toString(response.getEntity().getContent()));
                     assertTrue(false);
