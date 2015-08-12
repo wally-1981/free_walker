7b99e16055b09145c30ee5084cb2778a11a9842f
wally.1981 <wally.1981@yahoo.com>
2015-08-12 23:10:07 +0800

    sync one-way ssl to all the server start scripts
    Signed-off-by: wally.1981 <wally.1981@yahoo.com>

--- server/rest/com.free.walker.service.itinerary/src/main/resources/startServerDevo.sh
+++ server/rest/com.free.walker.service.itinerary/src/main/resources/startServerDevo.sh
@@ -5,17 +5,18 @@
 
 # Create the combination keystore/truststore for the client and service.
 # Note you can create separate keystores/truststores for both if desired
-keytool -genkeypair -validity 365 -alias myservicekey -keystore ServiceKeystore.jks -dname "cn=127.0.0.1" -keypass skpass -storepass sspass
-keytool -genkeypair -validity 365 -alias myclientkey -keystore ClientKeystore.jks -keypass ckpass -storepass cspass
+keytool -genkeypair -validity 365 -v -alias myservicekey -keystore ServiceKeystore.jks -keyalg RSA -storetype JKS -dname "cn=127.0.0.1,ou=TechDept,o=FreeWalker,l=Haidian,st=Beijing,c=CN" -keypass skpass -storepass sspass
+keytool -genkeypair -validity 365 -v -alias myclientkey -keystore ClientKeystore.jks -keyalg RSA -storetype JKS -dname "cn=ServiceClient,ou=TechDept,o=FreeWalker,l=Haidian,st=Beijing,c=CN" -keypass ckpass -storepass cspass
 
 # Place server public cert in client key/truststore
-keytool -export -rfc -keystore ServiceKeystore.jks -alias myservicekey -file MyService.cer -storepass sspass
+keytool -export -rfc -v -keystore ServiceKeystore.jks -alias myservicekey -file MyService.cer -storepass sspass
 keytool -import -noprompt -trustcacerts -file MyService.cer -alias myservicekey -keystore ClientKeystore.jks -storepass cspass
 
 # Place client public cert in service key/truststore
 # Note this needs to be done only if you're requiring client authentication as configured in resources/ServerConfig.xml
-keytool -export -rfc -keystore ClientKeystore.jks -alias myclientkey -file MyClient.cer -storepass cspass
+keytool -export -rfc -v -keystore ClientKeystore.jks -alias myclientkey -file MyClient.cer -storepass cspass
 keytool -import -noprompt -trustcacerts -file MyClient.cer -alias myclientkey -keystore ServiceKeystore.jks -storepass sspass
+
 
 # Place the keystore and cert files before starting the server.
 if [ ! -d keystore ]; then
