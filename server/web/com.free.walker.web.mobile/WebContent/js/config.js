98565ed47053b1f0fda6627754228485865bb38b
wally.1981 <wally.1981@yahoo.com>
2015-11-24 23:23:32 +0800

    checkin the web mobile project skeleton and add i18n support
    Signed-off-by: wally.1981 <wally.1981@yahoo.com>

--- server/web/com.free.walker.web.mobile/WebContent/js/config.js
+++
@@ -1,4 +0,0 @@
-basicConfig = {
-    baseURL: "https://192.168.1.100:9001/service/",
-    credential: 'Basic ' + window.btoa('admin:passw0rd')
-}
---
+++ server/web/com.free.walker.web.mobile/WebContent/js/main.js
@@ -0,0 +1,41 @@
+/**
+ * The Main
+ */
+$(document).ready(function() {
+    /*
+     * load the i18n strings for localization support.
+     */
+    $.i18n.properties({
+        name: 'nls_strings',
+        path: '/i18n/',
+        mode: 'both',
+        callback: function() {
+            $('title').html($.i18n.prop('main_title'));
+            $('h3#main_title').html($.i18n.prop('main_title'));
+
+            $('a#home_sec').html($.i18n.prop('home'));
+            $('a#customize_sec').html($.i18n.prop('customize'));
+            $('a#mine_sec').html($.i18n.prop('mine'));
+        }
+    });
+
+    /*
+     * 
+     */
+    var navigatable_nav_item = {
+        'a#home_sec' : 'div#home',
+        'a#customize_sec' : 'div#customize',
+        'a#mine_sec' : 'div#mine'
+    };
+    _.each(_.keys(navigatable_nav_item), function(element, index, list) {
+        $(element).click(function() {
+            _.each(list, function(element) {
+                $(element).parent().removeClass('active');
+                $(navigatable_nav_item[element]).addClass('hidden');
+            });
+
+            $(this).parent().addClass('active');
+            $(navigatable_nav_item[element]).removeClass('hidden');
+        });
+    });
+});
